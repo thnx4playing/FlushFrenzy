360e36ad3051a88639a0b02fcfdd3fe24833db6a
thnx4playing <mpengle@gmail.com>
2025-08-10 21:48:53 -0400

    Remove temporary debug dump - walls are working again
    - Removed the temporary debug dump since walls are back
    - Physics collision is working properly again
    - TP now bounces off walls and hits toilet properly
    - Clean console output maintained
    - Game is fully functional

--- src/games/ToiletPaperToss.js
+++ src/games/ToiletPaperToss.js
@@ -590,11 +590,6 @@ export default function ToiletPaperToss({ onGameComplete, gameMode }) {
     const id = setInterval(() => setTick((t) => t + 1), 33);
     setReady(true);
     
-    // Debug: Verify walls are created (run once)
-    setTimeout(() => {
-      dumpStatics(engine);
-    }, 500);
-    
     return () => clearInterval(id);
   }, []);
 
