13a002f2398b75b3816425b3cefb5ffa38e6a8f7
Mike <mike@thnx4playing.com>
2026-02-24 08:35:42 -0500

    Bump iOS build number to 12 for TestFlight

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ module.exports = {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.thnx4playing.FlushFrenzy",
-      buildNumber: "11",
+      buildNumber: "12",
       icon: path.resolve(__dirname, 'assets/default/app-icon.png'),
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
